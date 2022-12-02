5a8a9498cc58131089721ae46913e1103cd39f2d
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-12-02 03:15:32 -0800

    2.2 completed

--- Stella/2/2.py
+++ Stella/2/2.py
@@ -1,16 +1,17 @@
 # Day 2: Rock Paper Scissors
-shapeScore = {
-  'X': 1,
-  'Y': 2,
-  'Z': 3
+outScore = {
+  'X': 0,
+  'Y': 3,
+  'Z': 6
 }
 
-# X for Rock, Y for Paper, and Z for Scissors.
+# X for Lose, Y for Draw,  and Z for Win.
 # A for Rock, B for Paper, and C for Scissors.
-outScore = {
-    'X' : { 'A': 3, 'B': 0, 'C': 6}, 
-    'Y' : { 'A': 6, 'B': 3, 'C': 0},
-    'Z' : { 'A': 0, 'B': 6, 'C': 3},
+# Socre:1,2,3
+shapeScore = {
+    'X' : { 'A': 3, 'B': 1, 'C': 2}, 
+    'Y' : { 'A': 1, 'B': 2, 'C': 3},
+    'Z' : { 'A': 2, 'B': 3, 'C': 1},
 }
 
 def main():
@@ -19,15 +20,12 @@ def main():
     file1 = open('Stella/2/input.txt', 'r')
     Lines = file1.readlines()
     
-    # print(Lines)
-    #Strips the newline character
     total = 0
 
     for line in Lines:
         x = line.strip('\n').split(' ')
-        total += shapeScore[x[1]] + outScore[x[1]][x[0]]
+        total += outScore[x[1]] + shapeScore[x[1]][x[0]]
 
     print('Total score if everything goes exactly according to my strategy guide is ' + str(total))
-    # print('The '+ str(maxIndex) +'th elf carryies the most Calories which is ' + str(max) + ' Calories.')
 
 main()
