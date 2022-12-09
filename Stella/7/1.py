d95e56ec937e13fad16ec37e0b2a722799e2b5b5
yinshinLi <93513827+yinshinLi@users.noreply.github.com>
2022-12-08 21:01:49 -0800

    day 7... damn

--- Stella/7/1.py
+++ Stella/7/1.py
@@ -1,36 +1,68 @@
-# Day 5: Supply Stacks
+# Day 7: No Space Left On Device
+
+from collections import defaultdict
 
 def main():
 
-    file1 = open('Stella/5/input.txt', 'r')
+    file1 = open('Stella/7/input.txt', 'r')
     Lines = file1.readlines()
     
-    stacks = []
-    real = []
-
-    count = 0
-    i=0
-    start = 0
-
+    # parent file
+    path = []
+    # dict of files
+    files = defaultdict(list)
+    # dict of file size
+    sizes = {}
     for line in Lines:
-        stacks.append(list(line))
-        count+=1
-        if line == '\n':
-            print('stop')
-            for n in range(len(stacks[(len(stacks)-2)])):
-                if stacks[(len(stacks)-2)][n].isnumeric():
-                    real.append([])
-                    for x in range(len(stacks)-1):
-                        if(stacks[len(stacks)-2-x][n].isalpha()):
-                            real[i].append(stacks[len(stacks)-2-x][n])
-                    i+=1
-            start = 1
-        if start == 1:
-            ins = line.split()
-            if len(ins)>1:
-                for j in range(int(ins[1])):
-                    real[int(ins[5])-1].append(real[int(ins[3])-1].pop())
-    for x in real:
-        print(x.pop())
+        # command line
+        # print(line[0])
+        args = line.split()
+        if args[0] == '$':
+            if args[1] == 'cd':
+                if args[2] == '..':
+                    path.pop()
+                else:
+                    path.append(args[2])
+            # pass ls
+        else: 
+            if(args[0]=='dir'):
+                try:
+                    files[path[len(path)-1]].append(args[1])
+                except KeyError:
+                    files[path[len(path)-1]]= args[1]
+            else:
+                try:
+                    sizes[path[len(path)-1]] = sizes[path[len(path)-1]] + args[0]
+                except:
+                    sizes[path[len(path)-1]] = args[0]
+    # print(files)
+    # print(sizes)
+    while(len(files))>0:
+        print('loop')
+        print(len(files.copy()))
+        print(files)
+        for par in files.copy():
+            for ch in files[par]:
+                if not ch in files.copy():
+                    try:
+                        # print('try')
+                        sizes[par] = sizes[par] + sizes[ch]
+                    except:
+                        print('ex')
+                        sizes[par] = sizes[ch]
+                    # print(files[par])
+                    # print(len(files[par]))
+                    files[par].remove(ch)
+                    # print(files[par])
+                    # print(len(files[par]))
+                # else:
+                    # print(len(files))
+            if len(files[par]) == 0:
+                print('delete')
+                print(len(files))
+                del files[par]                
+                print(len(files))
+        
+        
 
 main()
--- Stella/7/2.py
+++ Stella/7/2.py
@@ -1,4 +1,4 @@
-# Day 5: Supply Stacks
+# Day 7: Supply Stacks
 
 def main():
 
